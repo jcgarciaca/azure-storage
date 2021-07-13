d50b277a22816dd60961dc60b0f9282dbcc99eb0
jcgarciaca <julio.jcgc@gmail.com>
2021-07-13 15:26:41 -0500

    GUI thread

--- GUI.py
+++ GUI.py
@@ -2,6 +2,8 @@ import tkinter
 from tkinter import filedialog
 import time
 import os
+import threading
+from queue import Queue
 
 class App:
     def __init__(self, window, window_title, video_source=0):
@@ -9,6 +11,7 @@ class App:
         self.logfile = None
         self.valid_folder = False
         self.valid_logfile = False
+        self.data = []
         self.background = '#d9d9d9'
         self.window = window
         self.window.title(window_title)
@@ -17,7 +20,7 @@ class App:
         self.window.columnconfigure(0, weight=1)
         self.window.columnconfigure(1, weight=1)
 
-        # Command buttons
+        # GUI
         self.btn_imgs_folder = tkinter.Button(window, text="Select Images Folder", width=20, command=self.search_folder)
         self.btn_imgs_folder.grid(column=0, row=0)
         self.btn_logfile = tkinter.Button(window, text="Select Log File", width=20, command=self.search_file)
@@ -29,11 +32,16 @@ class App:
         self.scrollbar.config(command=self.imgs_lb.yview)        
         self.imgs_lb.grid(column=0, row=2)
         self.scrollbar.grid(column=0, row=2, sticky=tkinter.NS+tkinter.E)
+
+        self.text_hmi = tkinter.StringVar()
+        self.text_hmi.set('')
+        self.lbl_hmi = tkinter.Label(window, textvariable=self.text_hmi, font=("Helvetica", 8))
+        self.lbl_hmi.grid(column=0, row=4, sticky='w')
         
         self.lbl_logfile = tkinter.Text(window, height=5, width=35, font=("Helvetica", 10), bg=self.background)
         self.lbl_logfile.grid(column=1, row=2)
 
-        self.btn_upload = tkinter.Button(window, text="Upload", width=20, command=self.upload_files)
+        self.btn_upload = tkinter.Button(window, text="Upload", width=20, command=self.upload_cmd)
         self.btn_upload.grid(column=1, row=3)
 
         self.window.mainloop()
@@ -57,16 +65,27 @@ class App:
             if os.path.isfile(self.logfile):
                 self.lbl_logfile.insert('end', 'Selected file:\n' + self.logfile)
                 f = open(self.logfile, 'r')
-                data = []
+                self.data = []
                 for line in f:
-                    data.append(line)
-                print('data:', len(data), data)
+                    self.data.append(line)
+                print('data:', len(self.data), self.data)
                 self.valid_logfile = True
 
-    def upload_files(self):
+    def upload_cmd(self):
         if self.valid_folder and self.valid_logfile:
-            print('Upload')
-        
+            queue = Queue()
+            thread = threading.Thread(target=self.upload_files, args=(queue,))
+            thread.start()
+
+    
+    def upload_files(self, queue):
+        try:
+            for idx, item in enumerate(self.data):
+                self.text_hmi.set(' Uploading {}/{}'.format(idx + 1, len(self.data)))
+                time.sleep(2.)
+            self.text_hmi.set(' Upload completed')
+        except:
+            self.text_hmi.set(' Upload failed')
 
 if __name__ == "__main__":
     # Create a window and pass it to the Application object
